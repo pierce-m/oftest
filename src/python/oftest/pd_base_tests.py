b1689c7b8ae00e06b5729514275c836161406cb0
Antonin Bas <antonin@barefootnetworks.com>
2015-03-31 18:59:11 -0700

    changed the way oftest plugs into the thrift python code

--- src/python/oftest/pd_base_tests.py
+++ src/python/oftest/pd_base_tests.py
@@ -5,6 +5,7 @@ Tests will usually inherit from one of these classes to have the controller
 and/or dataplane automatically set up.
 """
 
+import importlib
 import os
 import logging
 import unittest
@@ -20,7 +21,7 @@ import oftest.dataplane as dataplane
 #
 ################################################################
 
-import pd_thrift.bfn_pd_rpc as bfn_pd_rpc
+#import pd_thrift.bfn_pd_rpc as bfn_pd_rpc
 from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
@@ -38,6 +39,9 @@ class ThriftBaseTest(unittest.TestCase):
         logging.info("** END TEST CASE " + str(self))
 
 class ThriftInterface(ThriftBaseTest):
+    def __init__(self, p4_pd_client_module_name):
+        ThriftBaseTest.__init__(self)
+        self.client_module = importlib.import_module(p4_pd_client_module_name)
 
     def setUp(self):
         ThriftBaseTest.setUp(self)
@@ -47,7 +51,7 @@ class ThriftInterface(ThriftBaseTest):
         self.transport = TTransport.TBufferedTransport(self.transport)
         self.protocol = TBinaryProtocol.TBinaryProtocol(self.transport)
 
-        self.client = bfn_pd_rpc.Client(self.protocol)
+        self.client = self.client_module.Client(self.protocol)
         self.transport.open()
 
     def tearDown(self):
@@ -60,6 +64,9 @@ class ThriftInterfaceDataPlane(ThriftInterface):
     """
     Root class that sets up the thrift interface and dataplane
     """
+    def __init__(self, p4_pd_client_module_name):
+        ThriftInterface.__init__(self, p4_pd_client_module_name)
+
     def setUp(self):
         ThriftInterface.setUp(self)
         self.dataplane = oftest.dataplane_instance
